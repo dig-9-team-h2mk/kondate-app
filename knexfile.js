88d9b45e229a486a6e96ef5cddc771f070184c34
naoki <naoki_matsumura@mail.toyota.co.jp>
2025-11-27 11:11:07 +0900

    knexfile,knex.js,migrations作成

---
+++ knexfile.js
@@ -0,0 +1,31 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      user: process.env.POSTGRES_USER || "postgres",
+      database: process.env.POSTGRES_DB || "kondate",
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: { directory: "./data/seeds" },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: { directory: "./data/seeds" },
+  },
+};
