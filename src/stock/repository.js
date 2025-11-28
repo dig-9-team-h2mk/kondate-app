03f5ac7c84b6b0e8e8474483a94caef63f9f7568
naoki <naoki_matsumura@mail.toyota.co.jp>
2025-11-28 12:03:41 +0900

    stock_foodのgetのテスト確認OK

--- src/stock/repository.js
+++ src/stock/repository.js
@@ -1,4 +1,10 @@
 function createStockRepository(knex) {
+  const getByFoodAndUser = async (userId, foodName) => {
+    return await knex.select("*").from("stock_food").where({
+      user_id: userId,
+      food_name: foodName,
+    });
+  };
   const insert = async (userId, foodName, quantity) => {
     return await knex("stock_food").insert({
       user_id: userId,
@@ -6,7 +12,8 @@ function createStockRepository(knex) {
       quantity: quantity,
     });
   };
-  return { insert };
+
+  return { insert, getByFoodAndUser };
 }
 
 module.exports = { createStockRepository };
