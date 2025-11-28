6ddfb5e22a89455799770d35cb25c949c293903e
naoki <naoki_matsumura@mail.toyota.co.jp>
2025-11-28 17:40:27 +0900

    service重複確認テスト完了

--- src/stock/service.js
+++ src/stock/service.js
@@ -1,8 +1,22 @@
 function createStockService(repository) {
-  const create = async (useId, stockFood, quantity) => {
-    return await repository.insert(useId, stockFood, quantity);
+  const create = async (userId, stockFood, quantity) => {
+    return await repository.insert(userId, stockFood, quantity);
   };
-  return { create };
+  const checkDuplication = async (userId, stockFood, quantity) => {
+    const result = await repository.getByFoodAndUser(userId, stockFood);
+    if (result.length === 0) {
+      create(userId, stockFood, quantity);
+      return {
+        message: "登録完了",
+      };
+    } else {
+      return {
+        message: "すでに存在しています",
+        value: result,
+      };
+    }
+  };
+  return { create, checkDuplication };
 }
 
 module.exports = { createStockService };
