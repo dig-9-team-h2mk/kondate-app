12af3843ee33c31b1f5a815b1dd459e788ba6802
shinya_kato_ae@mail.toyota.co.jp <shinya_kato_ae@mail.toyota.co.jp>
2025-11-28 14:14:29 +0900

    package.jsonのビルドコマンドにバックエンドのnpm i　とマイグレーションを追加、　server.jsのstaticのパスを修正

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(__dirname, +"/public"));
+console.log(path.join(__dirname, '../public'));
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, () => {
   console.log(`サーバー立ち上がりました ${PORT}`);
